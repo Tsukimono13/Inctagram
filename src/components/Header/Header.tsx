0543d65189540e3d6a7ebbdd3562702c6bc27241
Tommy <dfgthii@gmail.com>
2023-05-08 19:49:44 +0300

    feat: (add layout, create Header, add globals style in globals.css)

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import s from './header.module.css'
+
+
+export const Header = () => {
+    return (
+        <div className={s.header}>
+            <h2 className={s.title}>Inctagram</h2>
+        </div>
+    )
+}
---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,17 @@
+import {NextPage} from "next";
+import {PropsWithChildren, ReactElement} from "react";
+import {Header} from "../Header/Header";
+import s from './layout.module.css'
+
+export const Layout: NextPage<PropsWithChildren> = (props) => {
+    const {children} = props
+
+    return (
+        <div className={s.container}>
+            <Header/>
+            <div>{children}</div>
+        </div>
+    );
+}
+
+export const getLayout = (page: ReactElement) =>  <Layout>{page}</Layout>
