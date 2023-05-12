d82ed3d87f90fdd4877f39e6773d64e478d18d78
Vladyslavua <vladgershman@gmail.com>
2023-05-12 16:17:49 +0200

    update: refactoring

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,5 +17,8 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
 
   const getLayout = Component.getLayout ?? ((page) => page);
 
-  return getLayout(<Provider store={store}><Component {...pageProps} /></Provider>)
+  return (
+      <Provider store={store}>
+        {getLayout(<Component {...pageProps} />)}
+      </Provider>)
 }
