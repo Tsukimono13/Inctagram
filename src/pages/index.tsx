0d4929d1a3bdbf56562dc6dc21430834efd64795
Vladyslavua <vladgershman@gmail.com>
2023-05-12 17:18:32 +0200

    update: routes from home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,24 @@
 import {NextPageWithLayout} from "@/pages/_app";
 import {getLayout} from "@/components/Layout/Layout";
+import {useAppSelector} from "@/hooks/useAppSelector";
+import {signedIn} from "@/features/authReducer/authSelectors";
+import {useRouter} from "next/router";
+import {useEffect} from "react";
 
 const Home: NextPageWithLayout = () => {
+
+
+    const isSignedIn = useAppSelector(signedIn)
+
+    const router = useRouter()
+
+    useEffect(() => {
+
+        isSignedIn ? router.push('/profile') : router.push('/auth/signIn')
+
+    }, [isSignedIn])
+
+
     return (
         <div>
             <h1>Hi Hello Yo-yo Hi</h1>
---
+++ src/pages/profile/index.tsx
@@ -0,0 +1,16 @@
+import {getLayout} from "@/components/Layout/Layout";
+
+const Profile = () => {
+
+    return(
+        <div>
+            Hello User
+        </div>
+    )
+
+
+}
+
+Profile.getLayout = getLayout
+
+export default Profile
