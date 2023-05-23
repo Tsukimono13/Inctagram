4a1f52b044e4d9b45d2a12f07e4c7d05505e7f45
LDS196 <lihachevd87@gmail.com>
2023-05-23 13:02:46 +0400

    feat: recovery-password

---
+++ src/pages/recovery-password/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import RecoveryPassword from "@/components/RecoveryPassword/RecoveryPassword";
+import {getLayout} from "@/components/Layout/Layout";
+
+const RecoveryPage = () => <RecoveryPassword/>
+
+
+RecoveryPage.getLayout=getLayout
+export default RecoveryPage;
