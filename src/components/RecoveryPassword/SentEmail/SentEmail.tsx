d6a58483fe2cb18b4f0b1cafe570dcb1b6ccaddb
LDS196 <lihachevd87@gmail.com>
2023-05-31 17:41:34 +0400

    add link

--- src/components/RecoveryPassword/SentEmail/SentEmail.tsx
+++ src/components/RecoveryPassword/SentEmail/SentEmail.tsx
@@ -10,12 +10,14 @@ import {useRouter} from "next/router";
 const SentEmail = () => {
   const router = useRouter()
   const email = (router.query?.email)
-
+  const onClickHandler = () => {
+    return router.push('/signIn')
+  }
   return (
     <div className={s.main}>
       <div className={s.title}>
         <TitleForAuth marginBottom={'0px'} text={'Email sent'}/>
-        <CloseIcon/>
+        <CloseIcon onClick={onClickHandler}/>
       </div>
       <div className={s.subTitle}>
         <TextForAuth text={`We have sent a link to confirm your email to ${email}`}
@@ -25,6 +27,7 @@ const SentEmail = () => {
         />
         <div style={{marginBottom: '6px', textAlign: 'end'}}>
           <ButtonBlue
+            callback={onClickHandler}
             disabled={false}
             title={'OK'}
             width={'96px'}
