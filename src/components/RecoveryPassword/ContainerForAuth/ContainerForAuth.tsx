7d15bff253f95ef83efd4cd45bd1824adbb70576
Irina Litvinova <neforsha@yandex.ru>
2023-06-01 20:31:46 +0300

    ref: refactoring

--- src/components/RecoveryPassword/ContainerForAuth/ContainerForAuth.tsx
+++ src/components/RecoveryPassword/ContainerForAuth/ContainerForAuth.tsx
@@ -1,23 +1,23 @@
 import {FC, ReactNode} from "react";
 
 type Props = {
-  children: ReactNode
-  background:string
-  border:string
+    children: ReactNode
+    background: string
+    border: string
 }
 
-export const ContainerForAuth: FC<Props> = ({children, background,border,}) => {
-  const style = {
-    maxWidth: '378px',
-    padding: '24px',
-    background: background,
-    border: border,
-    borderRadius: '2px',
-    margin: '72px auto 0 auto'
-  }
-  return (
-    <div style={style}>
-      {children}
-    </div>
-  );
+export const ContainerForAuth: FC<Props> = ({children, background, border,}) => {
+    const style = {
+        maxWidth: '378px',
+        padding: '24px',
+        background: background,
+        border: border,
+        borderRadius: '2px',
+        margin: '72px auto 0 auto'
+    }
+    return (
+        <div style={style}>
+            {children}
+        </div>
+    );
 }
