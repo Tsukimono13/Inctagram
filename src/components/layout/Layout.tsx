9dd5e03d22ecd3e9d8864ac2e60b8913ac208477
Vladyslavua <vladgershman@gmail.com>
2023-06-19 15:14:14 +0200

    update: Refactoring files name

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,6 @@
 import {NextPage} from "next";
 import { PropsWithChildren, ReactElement, ReactNode } from "react";
-import {Header} from "../header/Header";
+import {Header} from "../Header/Header";
 import s from './layout.module.css'
 
 
