7d8e897d4174cc7fed57d27069d1b6d1993e1cc6
Vladyslavua <vladgershman@gmail.com>
2023-07-08 15:40:32 +0200

    init:add crop for post components

---
+++ src/components/post/cropping/CropEasy.tsx
@@ -0,0 +1,120 @@
+import s from "./cropEasy.module.css";
+import {FC, useEffect, useState} from "react";
+import Cropper, {Area} from "react-easy-crop";
+import {getImageDimensions} from "@/assets/utils/getImage/getImageDimension";
+import getCroppedImg from "@/assets/utils/cropImage/cropImage";
+import {CropPhoto} from "@/components/post/Cropping/CropPhoto";
+import {HeaderForPost} from "@/components/post/headerForPost/HeaderForPost";
+import {useFile} from "@/hooks/fileContext/FileContext";
+import {FlagType} from "@/components/post/createPost/CreatePost";
+
+type PropsType = {
+    setFlag: (flag: FlagType) => void
+}
+
+export type SizeType = {
+    width: number
+    height: number
+}
+
+type CropType = {
+    x: number
+    y: number
+}
+
+export const CropEasy: FC<PropsType> = ({ setFlag }) => {
+
+    const [fileState,setFileState] = useFile()
+    const photoURL = fileState.urlOriginalFile
+
+    const [croppedAreaPixels, setCroppedAreaPixels] = useState<(SizeType & CropType) | null>(null)
+    const [currentSize, setCurrentSize] = useState<SizeType>({ height: 1, width: 1 })
+    const [originalSize, setOriginalSize] = useState<SizeType>({} as SizeType)
+    const [crop, setCrop] = useState<CropType>({ x: 0, y: 0 })
+    const [editPhoto, setEditPhoto] = useState(false)
+    const [openMenu, setOpenMenu] = useState(false)
+    const [zoom, setZoom] = useState(1)
+
+    const onCropComplete = (croppedArea: Area, croppedAreaPixels: Area) => {
+        setCroppedAreaPixels(croppedAreaPixels)
+    }
+
+    const onCropChange = (crop: CropType) => {
+        setCrop(crop)
+    }
+
+    const onZoomChange = (zoom: number) => {
+        setZoom(zoom)
+    }
+
+    const editPhotoHandler = () => {
+        setEditPhoto(!editPhoto)
+        setCrop({ x: 0, y: 0 })
+        setZoom(1)
+        if (openMenu) {
+            setOpenMenu(false)
+        }
+    }
+
+    const openModalHandler = () => {
+        setOpenMenu(!openMenu)
+        if (editPhoto) {
+            setEditPhoto(false)
+        }
+    }
+
+    const cropImage = async () => {
+        const res = await getCroppedImg(photoURL, croppedAreaPixels)
+        if (res) {
+            setFileState(state => ({...state,urlCropFile:res.url}))
+            setFileState(state => ({...state,cropFile:res.file as File}))
+            setFlag('filter')
+        }
+    }
+
+    const originalPic = () => {
+        setZoom(1)
+        setCurrentSize(originalSize)
+    }
+
+    useEffect(() => {
+        getImageDimensions(fileState.originalFile).then(res => {
+            setOriginalSize({ width: res.width, height: res.height })
+            setCurrentSize({ width: res.width, height: res.height })
+        })
+    }, [fileState.originalFile])
+
+
+    return (
+        <div>
+            <HeaderForPost
+                callbackForBack={()=> setFlag('load')}
+                callbackForNextStep={cropImage}
+                title={'Cropping'}
+                titleForNextStep={'Next'}
+                />
+            <div className={s.wrapper}>
+                <Cropper
+                    image={photoURL}
+                    showGrid={false}
+                    crop={crop}
+                    zoom={zoom}
+                    aspect={currentSize.width / currentSize.height}
+                    onZoomChange={onZoomChange}
+                    onCropChange={onCropChange}
+                    onCropComplete={onCropComplete}
+                />
+            </div>
+            <CropPhoto
+                editPhoto={editPhoto}
+                openMenu={openMenu}
+                zoom={zoom}
+                setZoom={setZoom}
+                editPhotoHandler={editPhotoHandler}
+                openModalHandler={openModalHandler}
+                originalPic={originalPic}
+                setCurrentSize={setCurrentSize}
+            />
+        </div>
+    )
+}
---
+++ src/components/post/cropping/CropPhoto.tsx
@@ -0,0 +1,99 @@
+import s from "./cropPhoto.module.css";
+import {SizeType} from "@/components/post/Cropping/CropEasy";
+import {FC} from "react";
+import expand from '../../../../public/icons/expand-outline.png'
+import maximize from '../../../../public/icons/maximize-outline.png'
+import image from '../../../../public/icons/image-outline.png'
+import format1x1 from '../../../../public/icons/format1x1.svg'
+import format5x4 from '../../../../public/icons/format5x4.svg'
+import format16x9 from '../../../../public/icons/format16x9.svg'
+import Image from "next/image";
+
+
+type PropsType = {
+    zoom: number
+    openMenu: boolean
+    editPhoto: boolean
+    originalPic: () => void
+    openModalHandler: () => void
+    editPhotoHandler: () => void
+    setZoom: (val: number) => void
+    setCurrentSize: (data: SizeType) => void
+}
+
+export const CropPhoto: FC<PropsType> = ({
+                                                      zoom,
+                                                      openMenu,
+                                                      editPhoto,
+                                                      setZoom,
+                                                      setCurrentSize,
+                                                      openModalHandler,
+                                                      editPhotoHandler,
+                                                      originalPic
+                                                  }) => {
+    const onZoomChange = (zoom: number) => {
+        setZoom(zoom)
+    }
+
+    const rectanglePicHandler = (size: SizeType) => {
+        setCurrentSize({ height: size.height, width: size.width })
+    }
+
+    return (
+        <div className={s.editPhoto}>
+            {editPhoto && (
+                <div className={s.inp}>
+                    <input
+                        type='range'
+                        min={1}
+                        max={3}
+                        step={0.1}
+                        value={zoom}
+                        onChange={e => onZoomChange(parseFloat(e.target.value))}
+                    />
+                </div>
+            )}
+            {openMenu && (
+                <div className={s.modal}>
+                    <label className={s.modalMain} onClick={originalPic}>
+                        Original
+                        <Image src={image} alt={'image'} className={s.image} />
+                    </label>
+                    <label
+                        className={s.modalMain}
+                        onClick={() => rectanglePicHandler({ height: 1, width: 1 })}
+                    >
+                        1:1
+                        <Image src={format1x1} alt={'rectangle1x1'} className={s.image} />
+                    </label>
+                    <label
+                        className={s.modalMain}
+                        onClick={() => rectanglePicHandler({ height: 5, width: 4 })}
+                    >
+                        4:5
+                        <Image src={format5x4} alt={'rectangle4x5'} className={s.image} />
+                    </label>
+                    <label
+                        className={s.modalMain}
+                        onClick={() => rectanglePicHandler({ height: 9, width: 16 })}
+                    >
+                        16:9
+                        <Image src={format16x9} alt={'rectangle16x9'} className={s.image} />
+                    </label>
+                </div>
+            )}
+            <div className={s.editButton}>
+                <Image src={expand} alt={'expand'} className={s.buttonChange} onClick={openModalHandler} />
+                <Image
+                    src={maximize}
+                    alt={'maximize'}
+                    className={`${s.buttonChange} ${s.maxi}`}
+                    onClick={editPhotoHandler}
+                />
+            </div>
+            <div className={s.editButton}>
+                <Image src={image} alt={'image'} className={s.buttonChange} />
+            </div>
+        </div>
+    )
+}
