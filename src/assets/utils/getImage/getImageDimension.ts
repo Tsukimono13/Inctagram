1992674796b23cabfc804f8c1bb2fbfe7f661220
Vladyslavua <vladgershman@gmail.com>
2023-07-08 15:34:34 +0200

    update:Added utils and react-easy-crop library for adding posts

---
+++ src/assets/utils/getImage/getImageDimension.ts
@@ -0,0 +1,20 @@
+import { SizeType } from '@/components/post/Cropping/CropEasy'
+
+
+export function getImageDimensions(file: File): Promise<SizeType> {
+    return new Promise(resolve => {
+        const reader = new FileReader()
+
+        reader.onload = function (e) {
+            const img = new Image()
+
+            img.onload = function () {
+                resolve({ width: img.width, height: img.height })
+            }
+            // @ts-ignore
+            img.src = e.target.result
+        }
+
+        reader.readAsDataURL(file)
+    })
+}
